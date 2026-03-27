8b5811b8951ef5cca1816836fe6a6083ab77a18b
anisebti-maker <anisebti@gmail.com>
2026-03-27 23:54:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // ← AJOUTER
+const cors = require('cors');
 const app = express();
 
-app.use(cors()); // ← AJOUTER
+app.use(cors());
 app.use(express.json());
 
 const TOKEN = "8690479197:AAHbBq3efzBLb0zSkqFbEO-4qgBdr1a_mKk";
-const CHAT_ID = "6055828369";
+const CHAT_ID = "1003562619802";
 
 app.post('/reservation', async (req, res) => {
-  const { name, phone, ds, de, car, msg, ins } = req.body;
+  const { name, phone, ds, de, car, price, total, msg, ins } = req.body;
+
   const text = `
 🚗 Nouvelle réservation Easyrent
-Véhicule : ${car}
-Client : ${name}
-Téléphone : ${phone}
-Du : ${ds}
-Au : ${de}
-Assurance : ${ins}
-Message : ${msg || "Aucun"}
+
+🚘 Véhicule : ${car}
+💵 Prix : ${price}
+📅 Du : ${ds}
+📅 Au : ${de}
+💰 Total estimé : ${total}
+🛡️ Assurance : ${ins}
+
+👤 Client : ${name}
+📞 Téléphone : ${phone}
+💬 Message : ${msg || "Aucun"}
 `;
+
   try {
     await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
       chat_id: CHAT_ID,
